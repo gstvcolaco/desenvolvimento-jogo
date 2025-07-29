a77e60971fefb986c016aa6d27fb24530f43f3b1
eduardajustus <eduardajustus46@gmail.com>
2025-07-29 14:56:56 -0300

    teste

--- PerguntasGame/core/src/main/java/com/info/perguntasgame/PerguntasGame.java
+++ PerguntasGame/core/src/main/java/com/info/perguntasgame/PerguntasGame.java
@@ -1,8 +1,6 @@
 package com.info.perguntasgame;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -22,7 +20,7 @@ public class PerguntasGame extends ApplicationAdapter {
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, 140, 210);
+        batch.draw(image, 140, 215);
         batch.end();
     }
 
