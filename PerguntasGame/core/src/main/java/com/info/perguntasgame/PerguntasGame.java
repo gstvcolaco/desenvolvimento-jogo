5b26edd4a70218c1d036b1e40c15031d309104d5
eduardajustus <eduardajustus46@gmail.com>
2025-08-01 15:29:10 -0300

    Tela

--- PerguntasGame/core/src/main/java/com/info/perguntasgame/PerguntasGame.java
+++ PerguntasGame/core/src/main/java/com/info/perguntasgame/PerguntasGame.java
@@ -9,18 +9,44 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class PerguntasGame extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
+    private Texture Bley;
+    private Texture BleyCarregar;
+    private Texture Cassio;
+    private Texture Cosmoski;
+    private Texture CosmoskiCarregar;
+    private Texture Diego;
+    private Texture DiegoCarregar;
+    private Texture Fundo;
+    private Texture Lucio;
+    private Texture LucioCarregar;
+    private Texture Pacheco;
+    private Texture PachecoCarregar;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
+        Bley = new Texture("Bley.png");
+        BleyCarregar = new Texture("BleyCarregar.png");
+        Cassio = new Texture("Cassio.png");
+        Cosmoski = new Texture("Cosmoski.png");
+        CosmoskiCarregar = new Texture("CosmoskiCarregar.png");
+        Diego = new Texture("Diego.png");
+        DiegoCarregar = new Texture("DiegoCarregar.png");
+        Fundo = new Texture("Fundo.png");
+        Lucio = new Texture("Lucio.png");
+        LucioCarregar = new Texture("LucioCarregar.png");
+        Pacheco = new Texture("Pacheco.png");
+        PachecoCarregar = new Texture("PachecoCarregar.png");
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, 140, 215);
+        batch.draw(Fundo, 00, 0,800,600);
+        batch.draw(Pacheco, 10,20,150,300);
+        batch.draw(Lucio, 480,20,150,320);
         batch.end();
     }
 
